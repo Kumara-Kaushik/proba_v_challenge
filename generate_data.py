c2b038824f031b7c7cc9b7995d858376012fb980
kumar15011995 <kumar@ascent.ai>
2019-11-25 21:32:22 +0900

    First commit

---
+++ generate_data.py
@@ -0,0 +1,109 @@
+import os
+import shutil
+import numpy as np
+from glob import glob
+import cv2 as cv2
+import matplotlib.pyplot as plt
+from urllib.request import urlretrieve
+from zipfile import ZipFile
+import fire
+import tqdm
+
+
+def prepare_dataset(root="./probav_data", download=False):
+
+    if download:
+        print("Downloading Dataset...")
+        urlretrieve('https://kelvins.esa.int/media/competitions/proba-v-super-resolution/probav_data.zip',
+                filename='probav_data.zip')
+        ZipFile('probav_data.zip').extractall('./probav_data/')
+        print("dataset downloaded and extracted!")
+
+    train_path = os.path.join(root, "train")
+    test_path = os.path.join(root, "test")
+
+    print("Preparing training data...")
+    save_images(train_path)
+    print("Training data preparation done!")
+    print("Preparing testing data...")
+    save_images(test_path)
+    print("Testing data preparation done!")
+
+def save_images(db_path):
+    hr_path = os.path.join(db_path, "HR_imgs")
+    lr_path = os.path.join(db_path, "LR_imgs")
+
+    if not os.path.exists(hr_path):
+        os.mkdir(hr_path)
+    if not os.path.exists(lr_path):
+        os.mkdir(lr_path)
+
+    path_walk = next(os.walk(db_path))
+
+    for path in [os.path.join(path_walk[0], i) for i in path_walk[1] if i in ["NIR", "RED"]]:
+        for img_folder_path in glob(path + "/" + "imgset*"):
+            scene_path = img_folder_path + "/"
+
+            hr_img_path = os.path.join(scene_path + "HR.png")
+            if os.path.exists(hr_img_path):
+                hr_img = cv2.imread(hr_img_path, 0)
+                hr_save_path = hr_path + "/" + img_folder_path[-10:] + ".png"
+                if not os.path.exists(hr_save_path):
+                    cv2.imwrite(hr_save_path, hr_img)
+                # shutil.copy(scene_path + "HR.png", hr_path + "/" + img_folder_path[-10:] + ".png")
+
+            images = []
+            for lr_img_path in glob(scene_path + 'LR*.png'):
+                lr_mask_path = lr_img_path.replace('LR', 'QM')
+                lr_img = cv2.imread(lr_img_path, 0)
+                lr_mask = cv2.imread(lr_mask_path, 0)
+                images.append((lr_img,lr_mask))
+
+            modified_lr_img = return_modified_lr_img(images)
+            lr_save_path = lr_path + "/" + img_folder_path[-10:] + ".png"
+            if not os.path.exists(lr_save_path):
+                cv2.imwrite(lr_save_path, modified_lr_img)
+            print(f"Folder {img_folder_path} Done!")
+
+def return_modified_lr_img(images, average=True):
+
+    (h, w)= images[0][0].shape
+    c = len(images)
+    img_array = np.zeros((h, w, c))
+    mask_array = np.zeros((h, w, c))
+    final_lr_img = np.zeros((h, w))
+
+    for i in range(c):
+        img_array[:, :, i] = images[i][0]
+        mask_array[:, :, i] = images[i][1]
+
+    best_channel = return_best_mask_channel(mask_array)
+    final_lr_img = img_array[:, :, best_channel]
+
+    for i in range(h):
+        for j in range(w):
+            if mask_array[i, j, best_channel] == 0.0:
+                mask_c_list = mask_array[i, j, :].tolist()
+                img_c_list = img_array[i, j, :].tolist()
+                if sum(mask_c_list) != 0:
+                    unobstructed_weight_list = [img_c_list[i] for i in range(len(img_c_list)) if mask_c_list[i] != 0]
+                    avg_weight = np.average(unobstructed_weight_list)
+                    median_weight = np.median(unobstructed_weight_list)
+                else:
+                    avg_weight = np.average(img_c_list)
+                    median_weight = np.median(img_c_list)
+                if average:
+                    final_lr_img[i, j] = int(avg_weight)
+                else:
+                    final_lr_img[i, j] = int(median_weight)
+    return final_lr_img
+
+
+def return_best_mask_channel(mask_array):
+    sum_dict = {i: mask_array[:, :, i].sum() for i in range(len(mask_array[0, 0, :]))}
+    best_mask_c = sorted(sum_dict.items(), key=lambda k: k[1])[-1][0]
+    return best_mask_c
+
+
+if __name__ == "__main__":
+    fire.Fire(prepare_dataset)
---
+++ predict.py
@@ -0,0 +1,73 @@
+import cv2 as cv2
+from fastai.vision import *
+import shutil
+import fire
+
+import skimage
+from skimage import io
+from skimage.transform import rescale
+
+class FeatureLoss(nn.Module):
+    def __init__(self, m_feat, layer_ids, layer_wgts):
+        super().__init__()
+        self.m_feat = m_feat
+        self.loss_features = [self.m_feat[i] for i in layer_ids]
+        self.hooks = hook_outputs(self.loss_features, detach=False)
+        self.wgts = layer_wgts
+        self.metric_names = ['pixel',] + [f'feat_{i}' for i in range(len(layer_ids))
+              ] + [f'gram_{i}' for i in range(len(layer_ids))]
+
+    def make_features(self, x, clone=False):
+        self.m_feat(x)
+        return [(o.clone() if clone else o) for o in self.hooks.stored]
+
+    def forward(self, input, target):
+        out_feat = self.make_features(target, clone=True)
+        in_feat = self.make_features(input)
+        self.feat_losses = [base_loss(input,target)]
+        self.feat_losses += [base_loss(f_in, f_out)*w
+                             for f_in, f_out, w in zip(in_feat, out_feat, self.wgts)]
+        self.feat_losses += [base_loss(gram_matrix(f_in), gram_matrix(f_out))*w**2 * 5e3
+                             for f_in, f_out, w in zip(in_feat, out_feat, self.wgts)]
+        self.metrics = dict(zip(self.metric_names, self.feat_losses))
+        return sum(self.feat_losses)
+
+    def __del__(self): self.hooks.remove()
+
+class SuperResolution():
+    def __init__(self, root_path="/home/kumar/.fastai/data/probav_data"):
+        self.proba_data = Path(root_path)
+        self.test_path_hr = self.proba_data.ls()[2]/'HR_imgs'
+        self.test_path_lr = self.proba_data.ls()[2]/'LR_imgs'
+        if os.path.exists(str(self.proba_data/'export.pkl')):
+            self._build_model()
+        else:
+            raise Exception(f"Given root path = {str(self.proba_data)} does not contain the export.pkl file!")
+
+    def _build_model(self):
+        learn = load_learner(self.proba_data/"train/LR_imgs")
+
+    def save_final_images(self):
+        for fn in self.test_path_lr.ls():
+            img = open_image(fn)
+            p,img_hr,b = learn.predict(img)
+            hr_pred_img = (img_hr.data.cpu().permute(1,2,0).numpy()[..., 1]*65535).astype(np.uint16)
+            hr_save_path = str(fn).replace("LR", "HR")
+            if not os.path.exists(hr_save_path):
+                cv2.imwrite(hr_save_path, hr_pred_img)
+
+    def make_submission_file(self):
+        shutil.make_archive(str(proba_data.ls()[2]/"submission"), 'zip', str(self.test_path_hr))
+
+    def save_bi_upsampled_imgs(self):
+        for fn in self.test_path_lr.ls():
+            mod_img = cv2.imread(str(fn), -1)
+            float_img = skimage.img_as_float(mod_img)
+            rescaled_img = rescale(float_img, scale=3, order=3, mode='edge', anti_aliasing=False, multichannel=False)
+            bi_img = (np.array(rescaled_img)*65535).astype(np.uint16)
+            hr_save_path = str(fn).replace("LR", "HR")
+            if not os.path.exists(hr_save_path):
+                cv2.imwrite(hr_save_path, bi_img)
+
+if "__name__"=="__main__":
+    fire.fire(SuperResolution)
